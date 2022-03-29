dfc290cb15c0c65df37b7ba4863fba28b55a2f60
MarKGit2021 <kuranovamarie2018@gmail.com>
2022-03-29 21:02:20 +0300

    Класс с комментариями

--- data/comments.py
+++ data/comments.py
@@ -1,22 +1,21 @@
 import datetime
 
 import sqlalchemy
-from flask_login import UserMixin
 from sqlalchemy import orm
-from sqlalchemy_serializer import SerializerMixin
 
 from .db_session import SqlAlchemyBase
 
 
-class Information(SqlAlchemyBase):
-    __tablename__ = 'information'
+class Comment(SqlAlchemyBase):
+    __tablename__ = 'comments'
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
     folder = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('users.id'), nullable=False)
-    points = sqlalchemy.Column(sqlalchemy.Integer, default=0)
+    information_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('information.id'), nullable=False)
     modified_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                       default=datetime.datetime.now)
-    user = orm.relation("User")
-
+    is_blocked = sqlalchemy.Column(sqlalchemy.Boolean, default=False)
+    information = orm.relation("Information")
+    user = orm.relation('User')
