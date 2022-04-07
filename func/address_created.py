24e3902ac7ff3ecec19d0ae41a6152b404762400
MarKGit2021 <kuranovamarie2018@gmail.com>
2022-04-07 21:10:11 +0300

    Комментарии

--- func/address_created.py
+++ func/address_created.py
@@ -2,9 +2,19 @@ from random import randint
 
 
 def address_created(object_id):
+    """
+    Метод, который генерирует адресс для страниц с информацией
+    :param object_id: int
+    :return: str
+    """
     one = str(randint(100, 999))
     return one + str(object_id)
 
 
 def get_id_for_address(folder):
+    """
+    Метод, который расшифровывает адресс страниц информации чтобы узнать id
+    :param folder: str
+    :return: int
+    """
     return int(str(folder)[3:])
